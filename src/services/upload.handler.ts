5ef1fa47e2fcf1492d8dfa7fc86216e213482073
MarcilioFarias <marcilio.m.farias@gmail.com>
2025-03-11 19:19:40 +1300

    adding lib sharp

--- src/services/upload.handler.ts
+++ src/services/upload.handler.ts
@@ -1,6 +1,17 @@
 import { RequestHandler } from "express";
+import sharp from "sharp";
 
-export const uploadImg: RequestHandler = (req, res) => {    
-    res.send('File uploaded');
-    console.log(req.file);
+export const uploadImg: RequestHandler = async (req, res) => {    
+    if(!req.file || (req.file && !req.file.mimetype.includes('image'))) {
+        res.status(400).send('Bad request');
+        return;
+    } else {
+        await sharp(req.file.path).resize(200, 200,
+            { fit: sharp.fit.cover,
+                position: 'center'
+            }
+        ).toFile(`./public/img/${req.file.filename}.png`);
+    }
+    res.status(201).json({ message: 'file uploaded', image: 'http://localhost:3000/img/'+req.file.filename+'.png' });
+    //console.log(req.file);
 };
